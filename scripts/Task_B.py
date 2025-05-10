a441d4d22aaf073cf90fb18a8eb41fba15f64b21
sovi2002 <svijayana@ethz.ch>
2025-05-10 16:37:10 +0200

    Update Task_B.py

--- scripts/Task_B.py
+++ scripts/Task_B.py
@@ -7,31 +7,37 @@ Created on Sat May 10 14:40:15 2025
 import cvxpy as cp
 import dynamiqs as dq
 import jax.numpy as jnp
-nx, ny = (6, 6)
-x_del, y_del = (1, 1)
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data_path = "C:/Users/Somesh/OneDrive/Documents/ETH/10._Semester/50_Fun/QHC/QHack25/data/experimental/wigner_fock_zero.pickle"
+data = pd.read_pickle(data_path)
+
 dim = 3
 
-x = jnp.linspace(-x_del, x_del, nx)
-y = jnp.linspace(-y_del, y_del, ny)
+x = data[0]
+y = data[1]
 xv, yv = jnp.meshgrid(x, y)
 
-def w(state, nx, x_del, y_del):
+def w(state, nx):
     return 1/2*(1+jnp.pi/2*dq.wigner(
         state,
-        xmax = x_del,
-        ymax = y_del,
         npixels = nx)[2])
 
-coherent = dq.coherent(dim, 0.5)
+#===========================================================================
+print('weights loading')
+# w_k = w(coherent, nx, x_del, y_del).flatten()
+w_k = data[2][::2, ::2].flatten()
+print(len(w_k))
 
-w_k = w(coherent, nx, x_del, y_del).flatten()
-
+#Define Observables
 alpha = (xv + yv*1j).flatten()
-E_list = [dq.eye(dim) + dq.displace(dim, alpha_k)@dq.parity(dim)@dq.dag(dq.displace(dim, alpha_k)) for alpha_k in alpha]
+E_list = 1/2*(dq.eye(dim) + dq.displace(dim, alpha)@dq.parity(dim)@dq.dag(dq.displace(dim, alpha)))
 
 # Define variables
 rho = cp.Variable((dim, dim), hermitian=True)
 
+print('Objectives Loading')
 # Objective
 residuals = [cp.real(cp.trace(E @ rho)) - p for E, p in zip(E_list, w_k)]
 objective = cp.Minimize(cp.sum_squares(cp.hstack(residuals)))
@@ -39,11 +45,18 @@ objective = cp.Minimize(cp.sum_squares(cp.hstack(residuals)))
 # Constraints
 constraints = [rho >> 0, cp.trace(rho) == 1]
 
+print('Problem Solving')
 # Solve problem
 prob = cp.Problem(objective, constraints)
-prob.solve(solver=cp.SCS)  # or use MOSEK if available
+prob.solve(solver=cp.MOSEK,
+           verbose = True)  # or use MOSEK if available
 
 # Output estimated density matrix
 rho_est = rho.value
 print("Estimated density matrix ρ:")
 print(rho_est)
+
+nx = len(data[2][::2, ::2])
+w_est = w(rho_est, nx)
+plt.imshow(w_est)
+plt.imshow(data[2][::2, ::2])
